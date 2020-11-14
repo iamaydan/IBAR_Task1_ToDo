b25ea52c3c252b9efb831cdbc118133556bd76d2
Aydan Gozalova <aydangozalova@gmail.com>
2020-11-14 23:44:00 +0400

    second

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -10,48 +10,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Initializing the array lists and the adapter
-        val itemlist = arrayListOf<String>()
-        val adapter = ArrayAdapter<String>(
-            this,
-            android.R.layout.simple_list_item_multiple_choice, itemlist
-        )
-        // Adding the items to the list when the add button is pressed
-        add.setOnClickListener {
 
-            itemlist.add(editText.text.toString())
-            listView.adapter = adapter
-            adapter.notifyDataSetChanged()
-            // This is because every time when you add the item the input space or the eidt text space will be cleared
+        val arrayAdapter: ArrayAdapter<*>
+        val listItems = arrayListOf<String>()
+        arrayAdapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, listItems)
+        listView.adapter = arrayAdapter
+
+        add.setOnClickListener {
+            listItems.add(editText.text.toString())
+            arrayAdapter.notifyDataSetChanged()
             editText.text.clear()
         }
-        // Clearing all the items in the list when the clear button is pressed
-        clear.setOnClickListener {
 
-            itemlist.clear()
-            adapter.notifyDataSetChanged()
+        listView.setOnItemClickListener { adapterView, view, i, j ->
+
+            arrayAdapter.notifyDataSetChanged()
         }
-        // Adding the toast message to the list when an item on the list is pressed
-        listView.setOnItemClickListener { adapterView, view, i, l ->
-            android.widget.Toast.makeText(
-                this,
-                "You Selected the item --> " + itemlist.get(i),
-                android.widget.Toast.LENGTH_SHORT
-            ).show()
-        }
-        // Selecting and Deleting the items from the list when the delete button is pressed
+
         delete.setOnClickListener {
             val position: SparseBooleanArray = listView.checkedItemPositions
             val count = listView.count
             var item = count - 1
             while (item >= 0) {
                 if (position.get(item)) {
-                    adapter.remove(itemlist.get(item))
+                    arrayAdapter.remove(listItems.get(item))
                 }
                 item--
             }
             position.clear()
-            adapter.notifyDataSetChanged()
+            arrayAdapter.notifyDataSetChanged()
         }
+
     }
 }
+
+
