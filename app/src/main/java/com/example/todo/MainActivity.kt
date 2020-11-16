6aa6a0c450bab85a3729aab6e344d7160428f038
Aydan Gozalova <aydangozalova@gmail.com>
2020-11-16 17:35:21 +0400

    clear list added

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -2,29 +2,32 @@ package com.example.todo
 
 import android.os.Bundle
 import android.util.SparseBooleanArray
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val listItems = arrayListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val arrayAdapter: ArrayAdapter<*>
-        val listItems = arrayListOf<String>()
         arrayAdapter =
             ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, listItems)
         listView.adapter = arrayAdapter
+
 
         add.setOnClickListener {
             listItems.add(editText.text.toString())
             arrayAdapter.notifyDataSetChanged()
             editText.text.clear()
+            textView2.visibility = View.INVISIBLE
         }
 
         listView.setOnItemClickListener { adapterView, view, i, j ->
-
+            Toast.makeText(this, "You clicked ${listItems[i]}", Toast.LENGTH_SHORT).show()
             arrayAdapter.notifyDataSetChanged()
         }
 
@@ -34,7 +37,7 @@ class MainActivity : AppCompatActivity() {
             var item = count - 1
             while (item >= 0) {
                 if (position.get(item)) {
-                    arrayAdapter.remove(listItems.get(item))
+                    arrayAdapter.remove(listItems[item])
                 }
                 item--
             }
@@ -42,6 +45,10 @@ class MainActivity : AppCompatActivity() {
             arrayAdapter.notifyDataSetChanged()
         }
 
+        clear.setOnClickListener {
+            arrayAdapter.clear()
+            textView2.visibility = View.VISIBLE
+        }
     }
 }
 
